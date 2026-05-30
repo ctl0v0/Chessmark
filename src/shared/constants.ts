3294c77966c966f7156591ad46149d295fe92200
Chris <59749849+ctl0v0@users.noreply.github.com>
2026-05-29 20:06:44 -0400

    Update extension version constant to 0.1.3

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
 export const BOOKMARKS_STORAGE_KEY = "positionBookmarks";
 export const TAG_PRESETS_STORAGE_KEY = "tagPresets";
-export const EXTENSION_VERSION = "0.1.2";
+export const EXTENSION_VERSION = "0.1.3";
 export const CHESS_COM_HOST = "www.chess.com";
